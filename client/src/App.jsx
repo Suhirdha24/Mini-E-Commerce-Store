a15815dfc6653fcdaeb22cec3c51dd543959e174
Suhirdha24 <suhirdhasivakumar@gmail.com>
2026-08-24 23:46:26 +0530

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-
-
-
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Product from "./pages/Product";
@@ -9,7 +7,6 @@ import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import Orders from "./pages/Orders";
 import OrderDetail from "./pages/OrderDetail";
-
 import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
 import Favorites from "./pages/Favorites";
@@ -19,72 +16,25 @@ import Admin from "./pages/Admin";
 function App() {
   return (
     <Routes>
-
       {/* Authentication */}
-      <Route
-        path="/login"
-        element={<Auth mode="login" />}
-      />
+      <Route path="/login" element={<Auth mode="login" />} />
+      <Route path="/register" element={<Auth mode="register" />} />
 
-      <Route
-        path="/register"
-        element={<Auth mode="register" />}
-      />
-
-      {/* Main Website */}
+      {/* Main Website Layout */}
       <Route element={<Layout />}>
-
         <Route path="/" element={<Home />} />
-
         <Route path="/shop" element={<Shop />} />
-
-        <Route
-          path="/product/:id"
-          element={<Product />}
-        />
-
-        <Route
-          path="/about"
-          element={<About />}
-        />
-
-        <Route
-          path="/favorites"
-          element={<Favorites />}
-        />
-
-        <Route
-          path="/cart"
-          element={<Cart />}
-        />
-
-        <Route
-          path="/checkout"
-          element={<Checkout />}
-        />
-
-        <Route
-          path="/profile"
-          element={<Profile />}
-        />
-
-        <Route
-          path="/orders"
-          element={<Orders />}
-        />
-
-        <Route
-          path="/orders/:id"
-          element={<OrderDetail />}
-        />
-
-        <Route
-          path="/admin"
-          element={<Admin />}
-        />
-
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/orders/:id" element={<OrderDetail />} />
+        <Route path="/admin" element={<Admin />} />
       </Route>
-
     </Routes>
   );
 }
